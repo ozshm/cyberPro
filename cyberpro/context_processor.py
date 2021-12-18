7f8508319481112a2581e4e7904c47d69454fd17
Oz Shmuel <oz.shmuel@ibm.com>
2021-12-18 23:33:58 +0200

    add pass-policy

--- cyberpro/context_processor.py
+++ cyberpro/context_processor.py
@@ -1,4 +1,7 @@
+from .settings import PASS_REQ
+
 def isauth_context_processor(request):
     return {
         'isAuthenticated': request.COOKIES.get('isAuthenticated'),
+        'pass_policy': PASS_REQ
     }
