1f769692e30760fd8c4b0a61628b65783cab6c5e
itziki95 <88631262+itziki95@users.noreply.github.com>
2021-12-28 20:20:00 +0200

    Working on password validation

--- users/views.py
+++ users/views.py
@@ -3,14 +3,53 @@ from django.shortcuts import render
 from .forms import UserForm, ChangePwdForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
+import json
+import os
 # Create your views here.
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+def load_user_create_requierments(path_to_req):
+    with open(os.path.join(BASE_DIR, path_to_req)) as file:
+        data = json.load(file)
+    return data
+
+def is_valid_password(password, password_repeat):
+    if password != password_repeat:
+        return False
+
+    count_digit = sum(c.isdigit() for c in password)
+    count_alpha = sum(c.isalpha() for c in password)
+    count_lower = sum(c.islower() for c in password)
+    count_upper = sum(c.isupper() for c in password)
+    count_special_char = 0
+    req = load_user_create_requierments("cyberpro/pass_req.json")
+    for special_char in req['password_content']['special_characters']:
+        count_special_char += password.count(special_char)
+
+    if req['min_length'] == len(password):
+        return False
+    if count_digit < req['password_content']['min_length_digit']:
+        return False
+    if count_alpha < req['password_content']['min_length_alpha']:
+        return False
+    if count_lower < req['password_content']['min_length_lower']:
+        return False
+    if count_upper < req['password_content']['min_length_upper']:
+        return False
+    if count_special_char < req['password_content']['min_length_special']:
+        return False
+    return True
+ 
 def user_create_view(request):
     form = UserForm(request.POST or None)
     if form.is_valid():
+        if not is_valid_password(form.cleaned_data['password'], form.cleaned_data['password_repeat']):
+            messages.info(request, "The password you entered does not meet the requirements, please try again.")
+            return HttpResponseRedirect('/register/')
         user = User.objects.create_user(
                     form.cleaned_data['username'],
                     form.cleaned_data['email'],
@@ -23,7 +62,7 @@ def user_create_view(request):
         form = UserForm()
     context = {
         'form': form,
-        'page_name': 'register',
+        'page_name': 'login',
     }
 
     return render(request, "users/user_create.html", context)
